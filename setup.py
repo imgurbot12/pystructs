ec415fb0d3e9199c92418bf1967b6bc810672389
imgurbot12 <imgurbot12@gmail.com>
2023-06-27 18:34:26 -0700

    chore: version upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pystructs3',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
